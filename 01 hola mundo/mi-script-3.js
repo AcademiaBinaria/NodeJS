4d5036533c96fb44fd8af54e6294fd67a68b8e24
Alberto Basalo <albertobasalo@hotmail.com>
2015-05-22 16:40:40 +0200

    errores

--- 01 hola mundo/mi-script-3.js
+++ 01 hola mundo/mi-script-3.js
@@ -1,24 +1,9 @@
-<<<<<<< HEAD
-"use strict";
-var nombre = 'Alberto';
-var edad = 43;
+   "use strict";
+   var nombre = 'Alberto';
+   edad = 43;
 
-function imprimir() {
-	console.log(nombre + " tiene " + edad + " años");
-}
+   function imprimir() {
+       console.log(nombre + " tiene " + edad + " años");
+   }
 
-imprimir();
-=======
-// Código encerrado en una función anónima
-(function () {
-//    "use strict";
-    var nombre = 'Alberto';
-    edad = 43;
-
-    function imprimir() {
-        console.log(nombre + " tiene " + edad + " años");
-    }
-
-    imprimir();
-}());// Invocación inmediata de funciones
->>>>>>> 106b62f3e1f7c97ae7aa25bc70f2c7362bd53abf
+   imprimir();
--- 01 hola mundo/proceso.js
+++ 01 hola mundo/proceso.js
@@ -1,38 +1,16 @@
-<<<<<<< HEAD
 "use strict";
 process.title = "mi super aplicación";
 imprimir();
 
 function imprimir() {
-	console.log('----------------------------------------');
-	console.log('   DATOS DE NODE.JS PROCESS');
-	console.log('----------------------------------------');
-	console.log('Directorio .......... ' + process.cwd());
-	console.log('Exec path ........... ' + process.execPath);
-	//console.log('identificador uid ... ' + process.getuid());
-	console.log('identificador pid ... ' + process.pid);
-	console.log('Título .............. ' + process.title);
-	console.log('Versión de Node  .... ' + process.version);
-	console.log('Plataforma........... ' + process.platform);
+    console.log('----------------------------------------');
+    console.log('   DATOS DE NODE.JS PROCESS');
+    console.log('----------------------------------------');
+    console.log('Directorio .......... ' + process.cwd());
+    console.log('Exec path ........... ' + process.execPath);
+    console.log('identificador uid ... ' + process.getuid());
+    console.log('identificador pid ... ' + process.pid);
+    console.log('Título .............. ' + process.title);
+    console.log('Versión de Node  .... ' + process.version);
+    console.log('Plataforma........... ' + process.platform);
 }
-=======
-(function () {
-	"use strict";
-	process.title = "mi super aplicación";
-	imprimir();
-
-	function imprimir() {
-		console.log('----------------------------------------');
-		console.log('   DATOS DE NODE.JS PROCESS');
-		console.log('----------------------------------------');
-		console.log('Directorio .......... ' + process.cwd());
-		console.log('Exec path ........... ' + process.execPath);
-		console.log('identificador uid ... ' + process.getuid());
-		console.log('identificador pid ... ' + process.pid);
-		console.log('Título .............. ' + process.title);
-		console.log('Versión de Node  .... ' + process.version);
-		console.log('Plataforma........... ' + process.platform);
-	}
-
-}());
->>>>>>> 106b62f3e1f7c97ae7aa25bc70f2c7362bd53abf
